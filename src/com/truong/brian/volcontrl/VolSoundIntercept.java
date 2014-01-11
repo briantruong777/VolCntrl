c670fc1e405d282872e62176a5a0eb81aa569dcb
briantruong777 <briantruong777@gmail.com>
2014-01-11 15:57:16 -0600

    Successful volume surpression along with repo maintenance

---
+++ src/com/truong/brian/volcontrl/VolSoundIntercept.java
@@ -0,0 +1,37 @@
+package com.truong.brian.volcontrl;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.util.Log;
+
+public class VolSoundIntercept extends BroadcastReceiver
+{
+  private int count = 0;
+  
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+	  int curRing = intent.getIntExtra("android.media.EXTRA_VOLUME_STREAM_TYPE", -1);
+	  int curVal = intent.getIntExtra("android.media.EXTRA_VOLUME_STREAM_VALUE", -1);
+    int prevVal = intent.getIntExtra("android.media.EXTRA_PREV_VOLUME_STREAM_VALUE", -1);
+    Log.i("blah", curRing + " " + curVal + " " + prevVal);
+	  if (curRing != -1 && curVal != -1 && prevVal != -1 && curVal == prevVal)
+    {
+      count++;
+      Log.i("blah", ""+count);
+      if (count <= 5)
+      {
+        AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        am.adjustStreamVolume(curRing, AudioManager.ADJUST_SAME, AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+        Log.i("blah", "trigger");
+      }
+      else
+      {
+        count = 0;
+        Log.i("blah", ""+count);
+      }
+    }
+	}
+}
